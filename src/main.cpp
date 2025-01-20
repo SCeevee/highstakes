9e0246e50a5c0572b73487ce1c0beb33683e06c6
dareckolo <115425277+dareckolo@users.noreply.github.com>
2025-01-19 18:59:17 -0600

    fixed the heading thing

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,70 @@
 #include "main.h"
+
+#include <cmath>
+
 #include "liblvgl/llemu.hpp"
 #include "pros/abstract_motor.hpp"
 #include "pros/llemu.hpp"
 #include "pros/motors.hpp"
 #include "pros/rtos.hpp"
-#include <cmath>
 #define pi 3.141592653589793
 #define as(a, b, c, d) for (auto a = b; a < c; a += d)
 #define de(a, b, c, d) for (auto a = b; a > c; a -= d)
 
-int sortedColor = 0; //0 = keep blue, 1 = keep red, 2 = none
-bool autonColor; bool autonSide;//T = blue, F = red; T = close, F = far
-const int wheelCirc = 220; // in mm
-const int driveEncoders = 300; // ticks per revolution
-const double trackWidth = 10.8 * 25.4; // conversion to mm
-int lbStates[3] = {0,100,200}; //list of all the states
-int lbState = 0; //current state it is in
-const int lbTotalStates = sizeof(lbStates) / sizeof(lbStates[0]); //total number of states
+int sortedColor = 0;  // 0 = keep blue, 1 = keep red, 2 = none
+bool autonColor;
+bool autonSide;                         // T = blue, F = red; T = close, F = far
+const int wheelCirc = 220;              // in mm
+const int driveEncoders = 300;          // ticks per revolution
+const double trackWidth = 10.8 * 25.4;  // conversion to mm
+int lbStates[3] = {0, 100, 200};        // list of all the states
+int lbState = 0;                        // current state it is in
+const int lbTotalStates =
+    sizeof(lbStates) / sizeof(lbStates[0]);  // total number of states
 
-pros::MotorGroup left ({1, 2, 3}, pros::MotorGearset::blue);
+pros::MotorGroup left({1, 2, 3}, pros::MotorGearset::blue);
 pros::MotorGroup right({4, 5, 6}, pros::MotorGearset::blue);
-pros::Motor roller (7,pros::MotorGearset::green);// i defined these for you guys according to discord but follow the rest according to shyam (P.S. move(127))
-pros::Motor chain (-8,pros::MotorGearset::blue);
-pros::Motor lb (9,pros::MotorGearset::blue);
+pros::Motor roller(
+    7, pros::MotorGearset::green);  // i defined these for you guys according to
+                                    // discord but follow the rest according to
+                                    // shyam (P.S. move(127))
+pros::Motor chain(-8, pros::MotorGearset::blue);
+pros::Motor lb(9, pros::MotorGearset::blue);
 
-pros::Rotation lbRotation (10); 
-pros::Imu inertial (11);
+pros::Rotation lbRotation(10);
+pros::Imu inertial(11);
 
 pros::adi::Pneumatics mogoLeft('a', false);
 pros::adi::Pneumatics mogoRight('b', false);
 
-
-pros::Controller ctrl (CONTROLLER_MASTER); //controller here
+pros::Controller ctrl(CONTROLLER_MASTER);  // controller here
 /**
  * A callback function for LLEMU's center button.
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_left_button(){
+void on_left_button() {
   sortedColor++;
-    if (sortedColor > 2) {
-      sortedColor = 0;
-    }
+  if (sortedColor > 2) {
+    sortedColor = 0;
+  }
 }
 
-void on_center_button() {
-  autonColor = !autonColor;
-}
+void on_center_button() { autonColor = !autonColor; }
 
-void on_right_button() {
-  autonSide = !autonSide;
-}
+void on_right_button() { autonSide = !autonSide; }
 
-void ring_detected(){}
+void ring_detected() {}
 
-void ring_not_detected(){}
+void ring_not_detected() {}
 
-
-
-//Moves the robot forward and backward
+// Moves the robot forward and backward
 void drive(int inchesDist, bool forward, int rpm) {
-  left.tare_position_all(); right.tare_position_all();
+  left.tare_position_all();
+  right.tare_position_all();
   double mmDist = inchesDist * 25.4;
-  double rotations = round(10*(mmDist / wheelCirc)) * 0.1;
+  double rotations = round(10 * (mmDist / wheelCirc)) * 0.1;
   double ticks = round(rotations * driveEncoders);
   double pause = (rotations / rpm) * 60000;
 
@@ -78,8 +79,9 @@ void drive(int inchesDist, bool forward, int rpm) {
   pros::delay(pause + 100);
 }
 
-void turn(double degrees, bool turnLeft, int rpm){
-  left.tare_position_all();right.tare_position_all();
+void turn(double degrees, bool turnLeft, int rpm) {
+  left.tare_position_all();
+  right.tare_position_all();
   double turnCirc = pi * trackWidth;
   double arcLen = (degrees / 360) * turnCirc;
   double rotations = arcLen / wheelCirc;
@@ -88,7 +90,7 @@ void turn(double degrees, bool turnLeft, int rpm){
 
   if (turnLeft) {
     left.move_absolute(-ticks, rpm);
-    right.move_absolute(ticks, rpm);  
+    right.move_absolute(ticks, rpm);
   } else {
     left.move_absolute(ticks, rpm);
     right.move_absolute(-ticks, rpm);
@@ -96,16 +98,15 @@ void turn(double degrees, bool turnLeft, int rpm){
 
   pros::delay(pause + 100);
 }
-void ladyBrownCycle(bool forward){
-  if(forward){
+void ladyBrownCycle(bool forward) {
+  if (forward) {
     lbState++;
-  }
-  else{
+  } else {
     lbState--;
   }
   lbState = lbState % lbTotalStates;
 }
-void ladyBrownSet(){
+void ladyBrownSet() {
   int lbsense = 1.5;
   int error = (lbStates[lbState] - lbRotation.get_position());
   int movePower = lbsense * error;
@@ -113,56 +114,66 @@ void ladyBrownSet(){
 }
 
 void toHeading(double degrees, int rpm) {
-    double kp = 0.5;
-    while(true) {
-        double error = degrees - inertial.get_heading();
-        double turnControl = kp * error;
-
-        left.move(turnControl * -1);
-        right.move(turnControl);
-
-        //to break out of while true
-        if(abs(error) <= 0.5) {
-            break;
-        }
+  double kp = 0.5;
+  while (true) {
+    double error =
+        degrees - inertial.get_heading();  // heading is clockwise adi :skull:
+    error = fmod(degrees, 360);
+    double turnControl = kp * error;
+    if (error < 180) {  // turn right
+      turnControl = kp * error;
+      left.move(turnControl);
+      right.move(-turnControl);
     }
-    pros::delay(100);
+    if (error > 180) {  // turn left
+      turnControl = kp * (360 - error);
+      left.move(-turnControl);
+      right.move(turnControl);
+    }
+
+    // to break out of while true
+    if (error <= 0.5 || error >= 359.5) {
+      break;
+    }
+  }
+  pros::delay(100);
 }
 
-//old turn func but hopefully better
+// old turn func but hopefully better
 void inertialTurn(double degrees, int rpm) {
-    double kp = 0.5;
-    //remember: sensor thinks CCW is negative, we say CCW is positive
-    //so every time we get the inertial sensor rotation we mult by -1
-    double initPos = -1 * inertial.get_rotation();
-    double finalPos = initPos + degrees;
-    while(true) {
-        double needToTurn = finalPos - (-1 * inertial.get_rotation());
-        double turnControl = kp * needToTurn;
+  double kp = 0.5;
+  // remember: sensor thinks CCW is negative, we say CCW is positive
+  // so every time we get the inertial sensor rotation we mult by -1
+  double initPos = -1 * inertial.get_rotation();
+  double finalPos = initPos + degrees;
+  while (true) {
+    double needToTurn = finalPos - (-1 * inertial.get_rotation());
+    double turnControl = kp * needToTurn;
 
-        left.move(turnControl * -1);
-        right.move(turnControl);
+    left.move(turnControl * -1);
+    right.move(turnControl);
 
-        //to break out of while true
-        if(abs(needToTurn) <= 0.5) {
-            break;
-        }
+    // to break out of while true
+    if (abs(needToTurn) <= 0.5) {
+      break;
     }
+  }
 
-    pros::delay(100);
+  pros::delay(100);
 }
 
-void mogoRetract(){
+void mogoRetract() {
   mogoLeft.retract();
   mogoRight.retract();
 }
 
-void mogoExtend(){
-  mogoLeft.extend();  
+void mogoExtend() {
+  mogoLeft.extend();
   mogoRight.extend();
 }
 
-// also add drive functions for auton since you got drivetrain done to get things more expeditied
+// also add drive functions for auton since you got drivetrain done to get
+// things more expeditied
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -170,37 +181,45 @@ void mogoExtend(){
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();// Sets up LLEMU (https://pros.cs.purdue.edu/v5/tutorials/topical/llemu.html)
-  pros::lcd::register_btn0_cb(on_left_button); pros::lcd::register_btn1_cb(on_center_button); pros::lcd::register_btn2_cb(on_right_button);
-  pros::lcd::print(0, "Hi"); pros::lcd::print(5, "Initialized");
-  pros::Task([]{
-    if(sortedColor == 0){//auton color info
-      pros::lcd::print(1, "LB: Sorting for BLUE"); ctrl.print(1, 0, "LB: Sorting for BLUE");
+  pros::lcd::
+      initialize();  // Sets up LLEMU
+                     // (https://pros.cs.purdue.edu/v5/tutorials/topical/llemu.html)
+  pros::lcd::register_btn0_cb(on_left_button);
+  pros::lcd::register_btn1_cb(on_center_button);
+  pros::lcd::register_btn2_cb(on_right_button);
+  pros::lcd::print(0, "Hi");
+  pros::lcd::print(5, "Initialized");
+  pros::Task([] {
+    if (sortedColor == 0) {  // auton color info
+      pros::lcd::print(1, "LB: Sorting for BLUE");
+      ctrl.print(1, 0, "LB: Sorting for BLUE");
     } else if (sortedColor == 1) {
-      pros::lcd::print(1, "LB: Sorting for RED"); ctrl.print(1, 0, "LB: Sorting for RED");
+      pros::lcd::print(1, "LB: Sorting for RED");
+      ctrl.print(1, 0, "LB: Sorting for RED");
     } else {
-      pros::lcd::print(1, "LB: Sorting for N/A"); ctrl.print(1, 0, "LB: Sorting for N/A");
+      pros::lcd::print(1, "LB: Sorting for N/A");
+      ctrl.print(1, 0, "LB: Sorting for N/A");
     }
 
-    if(autonColor) {
+    if (autonColor) {
       pros::lcd::print(2, "CB: BLUE side auton");
     } else {
       pros::lcd::print(2, "CB: RED side auton");
     }
 
-    if(autonSide) {
+    if (autonSide) {
       pros::lcd::print(3, "RB: CLOSE side auton");
     } else {
       pros::lcd::print(3, "RB: FAR side auton");
     }
-    //insert temperature flags when all the motors are defined
+    // insert temperature flags when all the motors are defined
   });
-  pros::Task([]{
-    ladyBrownSet(); //rotates the lady brown thing to the state
+  pros::Task([] {
+    ladyBrownSet();  // rotates the lady brown thing to the state
   });
-  /*It's good to have an lcd layout to give flags etc to the driver; you can do this through pros::lcd::print() which is to the brain or
-  ctrl.print() which is to the controller, it's up to you to decide where!
-  (example from mentor code):
+  /*It's good to have an lcd layout to give flags etc to the driver; you can do
+  this through pros::lcd::print() which is to the brain or ctrl.print() which is
+  to the controller, it's up to you to decide where! (example from mentor code):
   lcd layout (max 8 lines):
   0: hi (can be changed/removed later)
   1: left button setting - color sort fling
@@ -209,7 +228,7 @@ void initialize() {
   4: temp flags - overheat or not
   5: comp ctrl mode flag - what mode it is in right now
   */
-	pros::lcd::register_btn1_cb(on_center_button);
+  pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -217,7 +236,7 @@ void initialize() {
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void disabled() {pros::lcd::print(5, "Disabled");}
+void disabled() { pros::lcd::print(5, "Disabled"); }
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -228,7 +247,7 @@ void disabled() {pros::lcd::print(5, "Disabled");}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {pros::lcd::print(5, "Competition Initialize");}
+void competition_initialize() { pros::lcd::print(5, "Competition Initialize"); }
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -241,7 +260,7 @@ void competition_initialize() {pros::lcd::print(5, "Competition Initialize");}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {pros::lcd::print(5, "Autonomous");}
+void autonomous() { pros::lcd::print(5, "Autonomous"); }
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -257,56 +276,66 @@ void autonomous() {pros::lcd::print(5, "Autonomous");}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-
   pros::lcd::print(5, "OpControl");
   while (true) {
-		//temp flags
-    float dtLeftOT = ((round(10.0*((left.get_temperature(0) + left.get_temperature(1) + left.get_temperature(2))/3.0)))/10.0);
-    float dtRightOT = ((round(10.0*((right.get_temperature(0) + right.get_temperature(1) + right.get_temperature(2))/3.0)))/10.0);
+    // temp flags
+    float dtLeftOT =
+        ((round(10.0 * ((left.get_temperature(0) + left.get_temperature(1) +
+                         left.get_temperature(2)) /
+                        3.0))) /
+         10.0);
+    float dtRightOT =
+        ((round(10.0 * ((right.get_temperature(0) + right.get_temperature(1) +
+                         right.get_temperature(2)) /
+                        3.0))) /
+         10.0);
     float chainOT = chain.get_temperature();
     float lbOT = lb.get_temperature();
     float rollerOT = roller.get_temperature();
-    //printing the overtemp flags on to lcd
-    pros::lcd::print(4, "DTL%.1f DTR%.1f Chain%.1f LB%.1f Roller%.1f", dtLeftOT, dtRightOT, chainOT, lbOT, rollerOT);
-		ctrl.print(0,0, "DTL%.1f DTR%.1f Chain%.1f LB%.1f Roller%.1f", dtLeftOT, dtRightOT, chainOT, lbOT, rollerOT);
+    // printing the overtemp flags on to lcd
+    pros::lcd::print(4, "DTL%.1f DTR%.1f Chain%.1f LB%.1f Roller%.1f", dtLeftOT,
+                     dtRightOT, chainOT, lbOT, rollerOT);
+    ctrl.print(0, 0, "DTL%.1f DTR%.1f Chain%.1f LB%.1f Roller%.1f", dtLeftOT,
+               dtRightOT, chainOT, lbOT, rollerOT);
     // Arcade control scheme
     int power = ctrl.get_analog(ANALOG_LEFT_Y);
     int turn;
-    if(ctrl.get_digital(DIGITAL_Y)) {// modifier
+    if (ctrl.get_digital(DIGITAL_Y)) {  // modifier
       turn = (ctrl.get_analog(ANALOG_RIGHT_X)) / 2;
-    }
-    else {
+    } else {
       turn = ctrl.get_analog(ANALOG_RIGHT_X);
     }
     int powerL = power + turn;
     int powerR = power - turn;
-      
-    //dt
+
+    // dt
     left.move(powerL);
     right.move(powerR);
 
-    if(ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
+    if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
       roller.move(127);
-    } if(ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+    }
+    if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
       roller.move(-128);
-    } if(ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+    }
+    if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
       chain.move(127);
-    } if(ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
+    }
+    if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
       chain.move(-128);
     }
 
-    if(ctrl.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)){
+    if (ctrl.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
       ladyBrownCycle(true);
     }
-    if(ctrl.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)){
+    if (ctrl.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)) {
       ladyBrownCycle(false);
     }
-    if(ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_A)){
+    if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
       mogoExtend();
-    }
-    else{
+    } else {
       mogoRetract();
     }
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+    pros::delay(20);  // Run for 20 ms then update
+  }
 }
