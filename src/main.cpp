725a3809359e71b47de2710eb9d85251725d2668
TurtleGod7 <adisriram7777@gmail.com>
2025-01-22 11:20:00 -0600

    Added lemlib and added skills.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -269,28 +269,8 @@ void initialize() {
 
 void disabled() { pros::lcd::print(5, "Disabled"); }
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() { pros::lcd::print(5, "Competition Initialize"); }
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void autonomous() { 
   pros::lcd::print(5, "Autonomous");// COLOR IS ACCOUNTED IN intake()
   if(autonElim){// Elimination auton here
@@ -311,19 +291,6 @@ void autonomous() {
   }
 }
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
   pros::lcd::print(5, "OpControl");
   while (true) {
